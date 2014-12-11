dbd1356303cbc2b152bb2149b1ca95b4f07fc4cf
Andreas / IMAGINARY <matt@mfo.de>
2014-12-11 15:17:45 +0100

    added first version of gallery slider
    (version from ICM Korea, August 2014)

---
+++ js/main.js
@@ -0,0 +1,185 @@
+jQuery(document).ready(function ($) {
+
+    // setup language switcher
+
+    function hideAllCaptionsExcept(classException){
+        $(".caption").each(function(){
+
+            $(this).children().each(function(){
+
+                if (!$(this).hasClass(classException)) {
+                    $(this).hide();
+                } else {
+                    $(this).show();
+                }
+            });
+        });
+    }
+
+    switcherButtons = $("#lang-switcher li");
+
+    function setButtonActive(button) {
+        switcherButtons.removeClass("active");
+        button.addClass("active");
+    }
+
+    switcherButtons.each(function(){
+       var theClass = $(this).attr("class");
+       theClass = theClass.replace('switcher-','');
+
+       $(this).click(function(){
+           hideAllCaptionsExcept(theClass);
+           setButtonActive($(this));
+       });
+    });
+
+
+    // activate english first
+
+    hideAllCaptionsExcept("lang-en");
+    setButtonActive($(".lang-switcher-en"));
+
+
+    // setup info button
+
+    $("#info-button").click(function(){
+        $("#lang-switcher, .caption").toggle();
+        $("#info-button").toggleClass("active");
+    });
+
+    $("#lang-switcher, .caption").hide();
+
+
+        setTimeout(function() {
+	    // setup window stuff
+	
+	    var windowHeight = $(window).height();
+	    var windowWidth = $(window).width();
+	
+	    $("#slider1_container, #slider1_container .slides").css({
+	        "width" : windowWidth,
+	        "height" : windowHeight
+	    });
+
+        var _CaptionTransitions = [];
+        _CaptionTransitions["L"] = { $Duration: 900, $FlyDirection: 1, $Easing: { $Left: $JssorEasing$.$EaseInOutSine }, $ScaleHorizontal: 0.6, $Opacity: 2 };
+        _CaptionTransitions["R"] = { $Duration: 900, $FlyDirection: 2, $Easing: { $Left: $JssorEasing$.$EaseInOutSine }, $ScaleHorizontal: 0.6, $Opacity: 2 };
+        _CaptionTransitions["T"] = { $Duration: 900, $FlyDirection: 4, $Easing: { $Top: $JssorEasing$.$EaseInOutSine }, $ScaleVertical: 0.6, $Opacity: 2 };
+        _CaptionTransitions["B"] = { $Duration: 900, $FlyDirection: 8, $Easing: { $Top: $JssorEasing$.$EaseInOutSine }, $ScaleVertical: 0.6, $Opacity: 2 };
+        _CaptionTransitions["ZMF|10"] = { $Duration: 900, $Zoom: 11, $Easing: { $Zoom: $JssorEasing$.$EaseOutQuad, $Opacity: $JssorEasing$.$EaseLinear }, $Opacity: 2 };
+        _CaptionTransitions["RTT|10"] = { $Duration: 900, $Zoom: 11, $Rotate: 1, $Easing: { $Zoom: $JssorEasing$.$EaseOutQuad, $Opacity: $JssorEasing$.$EaseLinear, $Rotate: $JssorEasing$.$EaseInExpo }, $Opacity: 2, $Round: { $Rotate: 0.8} };
+        _CaptionTransitions["RTT|2"] = { $Duration: 900, $Zoom: 3, $Rotate: 1, $Easing: { $Zoom: $JssorEasing$.$EaseInQuad, $Opacity: $JssorEasing$.$EaseLinear, $Rotate: $JssorEasing$.$EaseInQuad }, $Opacity: 2, $Round: { $Rotate: 0.5} };
+        _CaptionTransitions["RTTL|BR"] = { $Duration: 900, $Zoom: 11, $Rotate: 1, $FlyDirection: 10, $Easing: { $Left: $JssorEasing$.$EaseInCubic, $Top: $JssorEasing$.$EaseInCubic, $Zoom: $JssorEasing$.$EaseInCubic, $Opacity: $JssorEasing$.$EaseLinear, $Rotate: $JssorEasing$.$EaseInCubic }, $ScaleHorizontal: 0.6, $ScaleVertical: 0.6, $Opacity: 2, $Round: { $Rotate: 0.8} };
+        _CaptionTransitions["CLIP|LR"] = { $Duration: 900, $Clip: 15, $Easing: { $Clip: $JssorEasing$.$EaseInOutCubic }, $Opacity: 2 };
+        _CaptionTransitions["MCLIP|L"] = { $Duration: 900, $Clip: 1, $Move: true, $Easing: { $Clip: $JssorEasing$.$EaseInOutCubic} };
+        _CaptionTransitions["MCLIP|R"] = { $Duration: 900, $Clip: 2, $Move: true, $Easing: { $Clip: $JssorEasing$.$EaseInOutCubic} };
+
+        var options = {
+            $FillMode: 2,                                       //[Optional] The way to fill image in slide, 0 stretch, 1 contain (keep aspect ratio and put all inside slide), 2 cover (keep aspect ratio and cover whole slide), 4 actuall size, default value is 0
+            $AutoPlay: true,                                    //[Optional] Whether to auto play, to enable slideshow, this option must be set to true, default value is false
+            $AutoPlayInterval: 4000,                            //[Optional] Interval (in milliseconds) to go for next slide since the previous stopped if the slider is auto playing, default value is 3000
+            $PauseOnHover: 0,                                   //[Optional] Whether to pause when mouse over if a slider is auto playing, 0 no pause, 1 pause for desktop, 2 pause for touch device, 3 pause for desktop and touch device, default value is 1
+
+            $ArrowKeyNavigation: true,   			            //[Optional] Allows keyboard (arrow key) navigation or not, default value is false
+            $SlideEasing: $JssorEasing$.$EaseOutQuint,          //[Optional] Specifies easing for right to left animation, default value is $JssorEasing$.$EaseOutQuad
+            $SlideDuration: 1200,                               //[Optional] Specifies default duration (swipe) for slide in milliseconds, default value is 500
+            $MinDragOffsetToSlide: 20,                          //[Optional] Minimum drag offset to trigger slide , default value is 20
+            //$SlideWidth: 600,                                 //[Optional] Width of every slide in pixels, default value is width of 'slides' container
+            //$SlideHeight: 300,                                //[Optional] Height of every slide in pixels, default value is height of 'slides' container
+            $SlideSpacing: 0, 					                //[Optional] Space between each slide in pixels, default value is 0
+            $DisplayPieces: 1,                                  //[Optional] Number of pieces to display (the slideshow would be disabled if the value is set to greater than 1), the default value is 1
+            $ParkingPosition: 0,                                //[Optional] The offset position to park slide (this options applys only when slideshow disabled), default value is 0.
+            $UISearchMode: 1,                                   //[Optional] The way (0 parellel, 1 recursive, default value is 1) to search UI components (slides container, loading screen, navigator container, arrow navigator container, thumbnail navigator container etc).
+            $PlayOrientation: 1,                                //[Optional] Orientation to play slide (for auto play, navigation), 1 horizental, 2 vertical, default value is 1
+            $DragOrientation: 1,                                //[Optional] Orientation to drag slide, 0 no drag, 1 horizental, 2 vertical, 3 either, default value is 1 (Note that the $DragOrientation should be the same as $PlayOrientation when $DisplayPieces is greater than 1, or parking position is not 0)
+
+            $CaptionSliderOptions: {                            //[Optional] Options which specifies how to animate caption
+                $Class: $JssorCaptionSlider$,                   //[Required] Class to create instance to animate caption
+                $CaptionTransitions: _CaptionTransitions,       //[Required] An array of caption transitions to play caption, see caption transition section at jssor slideshow transition builder
+                $PlayInMode: 1,                                 //[Optional] 0 None (no play), 1 Chain (goes after main slide), 3 Chain Flatten (goes after main slide and flatten all caption animations), default value is 1
+                $PlayOutMode: 3                                 //[Optional] 0 None (no play), 1 Chain (goes before main slide), 3 Chain Flatten (goes before main slide and flatten all caption animations), default value is 1
+            },
+
+            $BulletNavigatorOptions: {                          //[Optional] Options to specify and enable navigator or not
+                $Class: $JssorBulletNavigator$,                 //[Required] Class to create navigator instance
+                $ChanceToShow: 2,                               //[Required] 0 Never, 1 Mouse Over, 2 Always
+                $AutoCenter: 1,                                 //[Optional] Auto center navigator in parent container, 0 None, 1 Horizontal, 2 Vertical, 3 Both, default value is 0
+                $Steps: 1,                                      //[Optional] Steps to go for each navigation request, default value is 1
+                $Lanes: 1,                                      //[Optional] Specify lanes to arrange items, default value is 1
+                $SpacingX: 8,                                   //[Optional] Horizontal space between each item in pixel, default value is 0
+                $SpacingY: 8,                                   //[Optional] Vertical space between each item in pixel, default value is 0
+                $Orientation: 1                                 //[Optional] The orientation of the navigator, 1 horizontal, 2 vertical, default value is 1
+            },
+
+            $ArrowNavigatorOptions: {                           //[Optional] Options to specify and enable arrow navigator or not
+                $Class: $JssorArrowNavigator$,                  //[Requried] Class to create arrow navigator instance
+                $ChanceToShow: 1,                               //[Required] 0 Never, 1 Mouse Over, 2 Always
+                $AutoCenter: 2,                                 //[Optional] Auto center arrows in parent container, 0 No, 1 Horizontal, 2 Vertical, 3 Both, default value is 0
+                $Steps: 1                                       //[Optional] Steps to go for each navigation request, default value is 1
+            }
+        };
+
+       window.jssor_slider1 = new $JssorSlider$("slider1_container", options);
+
+	function mouseeventhappened() {
+		window.jssor_slider1.$Pause();
+		if( !( typeof window.jssor_slider1_timeout  === 'undefined' ) )
+			clearTimeout( window.jssor_slider1_timeout );
+		window.jssor_slider1_timeout = setTimeout( window.jssor_slider1.$Play, 60000  );
+	}
+
+	$("body").mouseup( mouseeventhappened );
+	$("body").mousedown( mouseeventhappened );
+	$("body").mousemove( mouseeventhappened );
+
+        //responsive code begin
+        //you can remove responsive code if you don't want the slider scales while window resizes
+        function ScaleSlider() {
+            var windowWidth = $(window).width();
+
+            if (windowWidth) {
+                var windowHeight = $(window).height();
+                var originalWidth = jssor_slider1.$GetOriginalWidth();
+                var originalHeight = jssor_slider1.$GetOriginalHeight();
+
+                var scaleWidth = windowWidth;
+                if (originalWidth / windowWidth > originalHeight / windowHeight) {
+                    scaleWidth = Math.ceil(windowHeight / originalHeight * originalWidth);
+                }
+
+                jssor_slider1.$SetScaleWidth(scaleWidth);
+            }
+            else
+                window.setTimeout(ScaleSlider, 30);
+        }
+
+        ScaleSlider();
+
+        if (!navigator.userAgent.match(/(iPhone|iPod|iPad|BlackBerry|IEMobile)/)) {
+            $(window).bind('resize', ScaleSlider);
+        }
+        else {
+            $(window).bind("orientationchange", ScaleSlider);
+        }
+        //responsive code end
+    }, 1500)
+
+
+});
+
+
+//jssor Extra Function
+function onChangeSlide() {
+    //console.log("stop all videos");
+    //$("video").get(0).pause();
+    $(".video-js").each(function(){
+
+        if (this.player) {
+            var temp = this.player;
+            setTimeout(function(){
+                console.log("pause");
+                temp.pause();
+            }, 100);
+        }
+    });
+}
---
+++ readme.txt
@@ -0,0 +1,2 @@
+Imaginary Slider is adapted from Jssor.Slider.FullPack
+http://www.jssor.com/
